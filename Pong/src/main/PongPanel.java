8d93aec8e619fbd23b50c23fe5d664f081979196
Samuel Leith <samuel.s.leith@gmail.com>
2024-08-22 13:20:37 +1200

    testing timer, updating and paint features

--- Pong/src/main/PongPanel.java
+++ Pong/src/main/PongPanel.java
@@ -6,13 +6,35 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+
 
  public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	 
+	 private final static Color BACKGROUND_COLOUR = Color.BLACK;
+
+	 private final static int TIMER_DELAY = 5;
+	 
 	public PongPanel() {
-		setBackground(Color.black);
+		setBackground(BACKGROUND_COLOUR);
+		
+		Timer timer = new Timer(TIMER_DELAY, this);
+			
+			timer.start();
 	}
-	 @Override
+	
+	
+	@Override
+	public void paintComponent(Graphics g) {
+	     super.paintComponent(g);
+	    g.setColor(Color.WHITE);
+	     g.fillRect(20, 20, 100, 100);
+	 }
+	
+	@Override
 	public void keyTyped(KeyEvent event) {
 		// TODO Auto-generated method stub
 		
@@ -36,4 +58,8 @@ import javax.swing.JPanel;
 		
 	}
 
+	public void update(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 }
